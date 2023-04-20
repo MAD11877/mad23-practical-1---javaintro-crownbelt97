68fe486062afb9cd5480f6351e039d98bdf7144b
Jeremy Chan <94418887+crownbelt97@users.noreply.github.com>
2023-04-20 12:56:14 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -4,20 +4,21 @@ public class Question4
 {
   public static void main(String[] args)
   {
-    /**
-     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
-     *    e.g.
-     *     > 5
-     *     *****
-     *     ****
-     *     ***
-     *     **
-     *     *
-     * 
-     * Hint: 2 loops are required. System.out.print(...) to print on single line.
-     */
-     
     Scanner in = new Scanner(System.in);
+
+    int rows=5;      
+//inner loop  
+for (int i= rows-1; i>=0 ; i--)  
+{  
+//outer loop  
+for (int j=0; j<=i; j++)  
+{  
+//prints star and space  
+System.out.print("*" + " ");  
+}  
+//throws the cursor in the next line after printing each line  
+System.out.println();  
+}
     
   }
 }
