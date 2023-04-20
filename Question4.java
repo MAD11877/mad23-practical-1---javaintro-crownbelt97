0d2ad250097ae3c6f37c8e3e5597572225a275eb
Jeremy Chan <94418887+crownbelt97@users.noreply.github.com>
2023-04-20 12:58:53 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -14,7 +14,7 @@ for (int i= rows-1; i>=0 ; i--)
 for (int j=0; j<=i; j++)  
 {  
 //prints star and space  
-System.out.print("*" + " ");  
+System.out.print("*");  
 }  
 //throws the cursor in the next line after printing each line  
 System.out.println();  
