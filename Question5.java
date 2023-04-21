db0479c3dce1c7e0696c2ba16cbc222b8c46b730
Jeremy Chan <94418887+crownbelt97@users.noreply.github.com>
2023-04-21 13:13:03 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -27,6 +27,37 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter num");
+    
+    int num = in.nextInt();
+
+    ArrayList<Integer> data = new ArrayList<>();
+    
+    for (int i = 0; i < num; i++)
+      {
+        int x = in.nextInt();
+        data.add(x);
+      }
+    
+    int maxcount = 0;
+    int max_freq = 0;
+    
+    for (int j = 0; j < data.size(); j++ )
+          {
+            int count = 0;
+
+            for (int y = 0; y < data.size(); y++) {
+              if(data.get(j) == data.get(y)) {
+                count++;
+              }
+            }
+
+            if (count > maxcount) {
+              maxcount = count;
+              max_freq = data.get(j);
+            }
+          }
+    System.out.println(max_freq);
     
   }
 }
